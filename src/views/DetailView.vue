5142b83f81abc28b93a7aa225b976da11fd06b84
Le Tu <tula@mic.com>
2023-02-15 13:16:28 +0700

    Init project

---
+++ src/views/DetailView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>detail</p>
+    <p>Data Input: {{ dataInput?.msg }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+
+interface Props {
+  msg: string;
+  bar?: number;
+}
+
+const dataInput = defineProps<Props>();
+const hasObject = ref();
+
+onMounted(() => {
+  console.log(dataInput.msg);
+  hasObject.value = false;
+
+});
+</script>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    <TheWelcome />
+    <DetailView :msg="'hello'" />
+  </main>
+</template>
+
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import DetailView from "@/views/DetailView.vue";
+</script>
